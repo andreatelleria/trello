403b2ee98fc311b3728fdae64757c597dd5aeb74
llandreall <andrea.telleria@gmail.com>
2017-11-20 13:29:36 -0500

    Archivos HTML, CSS y Javascript añadidos

---
+++ js/app.js
@@ -0,0 +1,6 @@
+window.addEventListener('load', function () {
+  /*Variable para que el formulario esté oculto al cargar página*/
+  var hidden = oculto = document.getElementById('formularioOculto').style.display = 'none';
+  var list = document.getElementById('list');
+
+});
