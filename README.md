ed960d3ad110079b75b7adab4be8bb03cf503716
llandreall <andrea.telleria@gmail.com>
2017-11-20 13:50:06 -0500

    Archivo README y nuevo evento en JS agregados

---
+++ README.md
@@ -0,0 +1,13 @@
+## Trello
+
+#### Descripción:
+Este repositorio contiene una réplica básica de la aplicación Trello para desktop. Pusimos en práctica nuestros conocimientos sobre eventos en Javascript para darle mayor interacción a nuestra página web.
+
+La página se verá de la siguiente manera en el navegador:
+
+![Sin titulo](assets/images/captura-.png)  
+
+#### Herramientas utilizadas:
+* HTML
+* CSS
+* Javascript
--- js/app.js
+++ js/app.js
@@ -1,6 +1,13 @@
 window.addEventListener('load', function () {
-  /*Variable para que el formulario esté oculto al cargar página*/
-  var hidden = oculto = document.getElementById('formularioOculto').style.display = 'none';
+  /*Variable para que el formulario esté oculto al cargar la página*/
+  var hidden = oculto = document.getElementById('hiddenForm').style.display = 'none';
   var list = document.getElementById('list');
 
+  /* Evento click */
+  list.addEventListener('click', function () {
+    /*Funcion para ocultar añadir comentario y desplegar formulario*/
+    document.getElementById('list').style.display = 'none';
+    var hiddenForm = document.getElementById('hiddenForm');
+    hiddenForm.style.display = 'inline-block';
+  });
 });
